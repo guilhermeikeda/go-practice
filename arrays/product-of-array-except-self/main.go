5593f714791ee76150969ac4dba225be82ea3244
Guilherme Ikeda <guilherme.ikeda@sumup.com>
2025-08-14 13:36:43 -0300

    chore: add product of array

---
+++ arrays/product-of-array-except-self/main.go
@@ -0,0 +1,25 @@
+package main
+
+func main() {
+
+}
+
+func productExceptSelf(nums []int) []int {
+	n := len(nums)
+	answer := make([]int, n)
+
+	// First pass: prefix products
+	answer[0] = 1
+	for i := 1; i < n; i++ {
+		answer[i] = answer[i-1] * nums[i-1]
+	}
+
+	// Second pass: suffix products
+	suffix := 1
+	for i := n - 1; i >= 0; i-- {
+		answer[i] *= suffix
+		suffix *= nums[i]
+	}
+
+	return answer
+}
