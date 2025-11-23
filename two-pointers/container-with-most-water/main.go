84ffde32de10c6262fcab3245e1e11d2f440cede
Guilherme Ikeda <guilherme.ikeda@sumup.com>
2025-11-23 10:00:22 -0300

    add container with most water

--- two-pointers/container-with-most-water/main.go
+++ two-pointers/container-with-most-water/main.go
@@ -1,29 +1,39 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
 func main() {
-	// Define the date you want to convert to epoch time
-	dateString := "2023-12-31"
-
-	// Parse the date string into a time.Time object
-	now := time.Now()
-
-	// Convert the date to epoch time
-	epochTime := now.Unix()
-	convertUnix(uint64(epochTime))
-	fmt.Println("Epoch time for", dateString, "is:", epochTime)
 }
 
-func convertUnix(epochTime uint64) {
-	// Convert the epoch time back to a time.Time object
-	t := time.Unix(int64(epochTime), 0)
+func maxArea(height []int) int {
+	// area= largura X altura
+	// largura = iX - iY
+	// altura min(height[iX], height[iY])
 
-	// Format the time as a string
-	formattedTime := t.Format("2006-01-02 15:04:05")
+	// começa no primeiro e no segundo
+	// calcula area, move segundo ponteiro até chegar no final calculando as áreas e mantendo a maior
 
-	fmt.Println("Converted time from epoch:", formattedTime)
+	if len(height) <= 1 {
+		return 0
+	}
+
+	var maxArea int
+	var ptrA int = 0
+	var ptrB int = len(height) - 1 // last element
+
+	for ptrA != ptrB {
+		width := ptrB - ptrA
+		heightx := min(height[ptrB], height[ptrA])
+		area := heightx * width
+
+		if area > maxArea {
+			maxArea = area
+		}
+
+		if height[ptrA] < height[ptrB] {
+			ptrA++
+		} else {
+			ptrB--
+		}
+	}
+
+	return maxArea
 }
--- two-pointers/container-with-most-water/main_test.go
+++ two-pointers/container-with-most-water/main_test.go
@@ -8,19 +8,23 @@ import (
 
 func Test_method(t *testing.T) {
 	type TestCase struct {
-		input  string
-		output string
+		input  []int
+		output int
 	}
 
 	tests := []TestCase{
 		{
-			input:  "",
-			output: "",
+			input:  []int{1, 1},
+			output: 1,
+		},
+		{
+			input:  []int{1, 8, 6, 2, 5, 4, 8, 3, 7},
+			output: 49,
 		},
 	}
 
 	for _, test := range tests {
-		output := method()
+		output := maxArea(test.input)
 		assert.Equal(t, test.output, output)
 	}
 }
